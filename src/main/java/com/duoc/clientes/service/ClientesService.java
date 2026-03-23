fed21792813e6a29ec2df5c385deaeb0317cccde
Juan Mora <raicerk@gmail.com>
2026-03-23 20:49:08 -0300

    feat: Se agrega metodos de servicio

--- src/main/java/com/duoc/clientes/service/ClientesService.java
+++ src/main/java/com/duoc/clientes/service/ClientesService.java
@@ -1,7 +1,27 @@
 package com.duoc.clientes.service;
 
+import com.duoc.clientes.model.ClientesModel;
+import com.duoc.clientes.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientesService {
+    @Autowired
+    private ClientesRepository clientesRepository;
+
+    public List<ClientesModel> getLibros() {
+        return clientesRepository.obtenerClientes();
+    }
+
+    public ClientesModel saveCliente(ClientesModel cliente) {
+        return clientesRepository.guardar(cliente);
+    }
+
+    public String deleteCliente(String correo) {
+        clientesRepository.eliminar(correo);
+        return "Cliente eliminado";
+    }
 }
